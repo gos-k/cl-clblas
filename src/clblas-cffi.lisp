86aff41ef82479221e4dd75e5f3d84d7ec981412
gos-k <mag4.elan@gmail.com>
2016-08-29 00:33:28 +0900

    add in-package

--- src/clblas-cffi.lisp
+++ src/clblas-cffi.lisp
@@ -4,6 +4,7 @@
 ;;; Do not make changes to this file unless you know what you are doing--modify
 ;;; the SWIG interface file instead.
 
+(in-package :cl-clblas)
 
 ;;;SWIG wrapper code starts here
 
